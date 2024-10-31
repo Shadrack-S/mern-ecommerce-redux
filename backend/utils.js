ab3f3711fffef94e7df95f78bf255ba60f49ade2
Shadrack S <shadrackshibu1@yahoo.com>
2024-10-31 23:46:23 +0530

    ramdom Id creating

---
+++ backend/utils.js
@@ -0,0 +1,9 @@
+//create a function to genrate a random Id Eg : SHIRT5822ID , ALEN2002KLF  any random id with integer and letters
+const generateId = (body) => {
+
+    //write code here and return as result
+
+    return result
+}
+
+module.exports = { generateId }
